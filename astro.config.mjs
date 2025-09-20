9787aaf8e056d5434ee8677af7b2530748c80015
kacueto <kacueto@uninorte.edu.co>
2025-09-19 21:10:56 -0500

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://novatransst.com',
+  site: 'https://novetrans.com',
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
